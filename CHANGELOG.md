4d3cf9ed00550a88c6b684fdf3920933cf3ab55c
Dmytro Karpovych <ZAYEC77@gmail.com>
2016-08-10 04:28:14 +0300

    refactoring

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,15 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
-## [Unreleased]
+## v1.0.0
 ### Changed 
 - Move DataTable options to protected array. Add __set and __get methods.
+
+## v1.0.1
+### Changed
+- Improve README
+
+
+## v1.0.2
+### Fixed
+- Server-side pagination
--- src/DataTableAction.php
+++ src/DataTableAction.php
@@ -13,6 +13,7 @@ use yii\base\Action;
 use yii\base\InvalidConfigException;
 use yii\data\ActiveDataProvider;
 use yii\db\ActiveQuery;
+use yii\web\Response;
 
 /**
  * Action for processing ajax requests from DataTables.
@@ -67,51 +68,19 @@ class DataTableAction extends Action
         $filterQuery
             ->offset(Yii::$app->request->getQueryParam('start', 0))
             ->limit(Yii::$app->request->getQueryParam('length', -1));
-        /* Begin of fix - serverSide pagination - get pagination from server side - Yii
-        $dataProvider = new ActiveDataProvider(['query' => $filterQuery, 'pagination' => false]);
-        */
-        $dataProvider = new ActiveDataProvider(['query' => $filterQuery, 'pagination' => ['pageSize' => Yii::$app->request->getQueryParam('length', 10)] ]);
-        // End of fix - serverSide pagination - get pagination from server side - Yii
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $dataProvider = new ActiveDataProvider(['query' => $filterQuery, 'pagination' => ['pageSize' => Yii::$app->request->getQueryParam('length', 10)]]);
+        Yii::$app->response->format = Response::FORMAT_JSON;
         try {
             $response = [
                 'draw' => (int)$draw,
                 'recordsTotal' => (int)$originalQuery->count(),
                 'recordsFiltered' => (int)$dataProvider->getTotalCount(),
-                /* Begin of fix - get actual data from server according to filters, offset and limit
-                'data' => $dataProvider->getModels(),
-                */
                 'data' => $filterQuery->all(),
-            	// End of fix - get actual data from server according to filters, offset and limit
             ];
         } catch (\Exception $e) {
             return ['error' => $e->getMessage()];
         }
         return $response;
-    }
-
-    /**
-     * @param ActiveQuery $query
-     * @param array $columns
-     * @param array $order
-     * @return ActiveQuery
-     */
-    public function applyOrder(ActiveQuery $query, $columns, $order)
-    {
-        if ($this->applyOrder !== null) {
-            return call_user_func($this->applyOrder, $query, $columns, $order);
-        }
-
-        foreach ($order as $key => $item) {
-            // Begin of fix - avoid failure on columns not being orderable
-        	if (array_key_exists('orderable', $columns[$item['column']]) && $columns[$item['column']]['orderable'] === 'false') {
-        		continue;
-        	}
-        	// End of fix - avoid failure on columns not being orderable
-            $sort = $item['dir'] == 'desc' ? SORT_DESC : SORT_ASC;
-            $query->addOrderBy([$columns[$item['column']]['data'] => $sort]);
-        }
-        return $query;
     }
 
     /**
@@ -135,6 +104,28 @@ class DataTableAction extends Action
                 $value = empty($search['value']) ? $column['search']['value'] : $search['value'];
                 $query->orFilterWhere(['like', $column['data'], $value]);
             }
+        }
+        return $query;
+    }
+
+    /**
+     * @param ActiveQuery $query
+     * @param array $columns
+     * @param array $order
+     * @return ActiveQuery
+     */
+    public function applyOrder(ActiveQuery $query, $columns, $order)
+    {
+        if ($this->applyOrder !== null) {
+            return call_user_func($this->applyOrder, $query, $columns, $order);
+        }
+
+        foreach ($order as $key => $item) {
+            if (array_key_exists('orderable', $columns[$item['column']]) && $columns[$item['column']]['orderable'] === 'false') {
+                continue;
+            }
+            $sort = $item['dir'] == 'desc' ? SORT_DESC : SORT_ASC;
+            $query->addOrderBy([$columns[$item['column']]['data'] => $sort]);
         }
         return $query;
     }
