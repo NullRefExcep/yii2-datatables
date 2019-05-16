335e857b054cad4cb40450dfcca9445d2b62d5f2
Alex Dantart <dantart@gmail.com>
2019-05-16 07:36:15 +0200

    Columns cells html options feature

---
+++ DataTable.php
@@ -0,0 +1,266 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Serhiy Vinichuk
+ * @license MIT
+ * @author Serhiy Vinichuk <serhiyvinichuk@gmail.com>
+ */
+
+namespace nullref\datatable;
+
+use yii\base\Widget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Inflector;
+use yii\helpers\Json;
+use yii\web\JsExpression;
+
+/**
+ * Class DataTable
+ * @package nullref\datatable
+ * Features
+ * @property bool $autoWidth Feature control DataTables' smart column width handling
+ * @property bool $deferRender Feature control deferred rendering for additional speed of initialisation.
+ * @property bool $info Feature control table information display field
+ * @property bool $jQueryUI Use markup and classes for the table to be themed by jQuery UI ThemeRoller.
+ * @property bool $lengthChange Feature control the end user's ability to change the paging display length of the table.
+ * @property bool $ordering Feature control ordering (sorting) abilities in DataTables.
+ * @property bool $paging Enable or disable table pagination.
+ * @property bool $processing Feature control the processing indicator.
+ * @property bool $scrollX Enable horizontal scrolling
+ * @property bool $scrollY Enable vertical scrolling
+ * @property bool $searching Feature control search (filtering) abilities
+ * @property bool $serverSide Enable server-side filtering, paging and sorting calculations
+ * @property bool $stateSave Restore table state on page reload
+ * @property array $language
+ * Options
+ * @property array $ajax Load data for the table's content from an Ajax source
+ * @property array $data Data to use as the display data for the table.
+ * @property array $columnDefs Set column definition initialisation properties.
+ * @property array $columns Set column specific initialisation properties.
+ * @property bool|int|array $deferLoading Delay the loading of server-side data until second draw
+ * @propert bool $destroy Destroy any existing table matching the selector and replace with the new options.
+ * @property int $displayStart Initial paging start point
+ * @property string $dom Define the table control elements to appear on the page and in what order
+ * @property array $lengthMenu Change the options in the page length `select` list.
+ * @property bool $orderCellsTop Control which cell the order event handler will be applied to in a column
+ * @property bool $orderClasses Highlight the columns being ordered in the table's body
+ * @property array $order Initial order (sort) to apply to the table
+ * @property array $orderFixed Ordering to always be applied to the table
+ * @property bool $orderMulti Multiple column ordering ability control.
+ * @property int $pageLength Change the initial page length (number of rows per page)
+ * @property string $pagingType Pagination button display options.
+ * @property string|array $renderer Display component renderer types
+ * @property bool $retrieve Retrieve an existing DataTables instance
+ * @property bool $scrollCollapse Allow the table to reduce in height when a limited number of rows are shown.
+ * @property array $search
+ * @property array $searchCols Define an initial search for individual columns.
+ * @property array $searchDelay Set a throttle frequency for searching
+ * @property int $stateDuration Saved state validity duration
+ * @property array $stripeClasses Set the zebra stripe class names for the rows in the table.
+ * @property int $tabIndex Tab index control for keyboard navigation
+ * Callbacks
+ * @property string $createdRow Callback for whenever a TR element is created for the table's body.
+ * @property string $drawCallback Function that is called every time DataTables performs a draw.
+ * @property string $footerCallback Footer display callback function.
+ * @property string $formatNumber Number formatting callback function.
+ * @property string $headerCallback Header display callback function.
+ * @property string $infoCallback Table summary information display callback.
+ * @property string $initComplete Initialisation complete callback.
+ * @property string $preDrawCallback Pre-draw callback.
+ * @property string $rowCallback Row draw callback.
+ * @property string $stateLoadCallback Callback that defines where and how a saved state should be loaded.
+ * @property string $stateLoaded State loaded callback.
+ * @property string $stateLoadParams State loaded - data manipulation callback
+ * @property string $stateSaveCallback Callback that defines how the table state is stored and where.
+ * @property string $stateSaveParams State save - data manipulation callback
+ */
+class DataTable extends Widget
+{
+    const COLUMN_TYPE_DATE = 'date';
+    const COLUMN_TYPE_NUM = 'num';
+    const COLUMN_TYPE_NUM_FMT = 'num-fmt';
+    const COLUMN_TYPE_HTML_NUM = 'html-num';
+    const COLUMN_TYPE_HTML_NUM_FMT = 'html-num-fmt';
+    const COLUMN_TYPE_STRING = 'string';
+
+    const PAGING_SIMPLE = 'simple';
+    const PAGING_SIMPLE_NUMBERS = 'simple_numbers';
+    const PAGING_FULL = 'full';
+    const PAGING_FULL_NUMBERS = 'full_numbers';
+
+    protected $_options = [];
+
+    public $id;
+    /**
+     * @var array Html options for table
+     */
+    public $tableOptions = [];
+    public $withColumnFilter;
+
+    /** @var bool  */
+    public $globalVariable = false;
+
+    public function init()
+    {
+        parent::init();
+        DataTableAsset::register($this->getView());
+        $this->initColumns();
+        $this->initData();
+    }
+
+    public function run()
+    {
+        $id = isset($this->id) ? $this->id : $this->getId();
+        echo Html::beginTag('table', ArrayHelper::merge(['id' => $id], $this->tableOptions));
+
+        echo Html::endTag('table');
+
+        $globalVariable = $this->globalVariable ? "window[$id] =" : '';
+        if ($this->withColumnFilter) {
+            $encodedParams = Json::encode($this->getParams());
+
+            $this->getView()->registerJs(
+                    <<<JS
+{
+    var params = ${encodedParams};
+    var table;
+    ${globalVariable} table = jQuery("#${id}").DataTable(params);
+    var filterRow = jQuery('<tr></tr>');
+    jQuery('#${id} thead tr th').each(function(i) 
+    {
+        var cell = jQuery('<td></td>')
+            .attr('colspan', jQuery(this).attr('colspan'))
+            .attr('class', jQuery(this).attr('class'))
+            .attr('tabindex', jQuery(this).attr('tabindex'))
+            .attr('aria-controls', jQuery(this).attr('aria-controls'))
+            .removeClass('sorting sorting_disabled')
+            .appendTo(filterRow);
+
+        if (params.columns && params.columns[i] && params.columns[i].renderFilter) {
+            cell.html(jQuery.isFunction(params.columns[i].renderFilter) ? params.columns[i].renderFilter(table) : params.columns[i].renderFilter);
+        }
+    });
+    jQuery('#${id} thead').append(filterRow);
+    jQuery('#${id} thead tr:eq(1) td').each( function (i) 
+    {
+        jQuery(':input', this).on('keyup change', function () {
+            if (table.column(i).search() !== jQuery(this).val()) {
+                table
+                    .column(i)
+                    .search(jQuery(this).val())
+                    .draw();
+            }
+        } );
+    } );
+}
+JS
+            );
+        } else {
+            $this->getView()->registerJs($globalVariable . 'jQuery("#' . $id . '").DataTable(' . Json::encode($this->getParams()) . ');');
+        }
+
+        if (isSet($this->_options['htmlOptions'])) {
+            $js = "";
+            foreach ($this->_options['htmlOptions'] as $key => $data) {
+                $column = $key + 1;
+                $attrs = "";
+                foreach ($data as $attribute => $value) {
+                    $attrs .= ".attr('".$attribute."', '".$value."')";
+                }
+                $js.= "jQuery('#".$id." tbody tr td:nth-child(".$column.")').each(function() { $(this)".$attrs."; });";
+            }
+
+            $this->getView()->registerJs(
+                        <<<JS
+{
+                        $('#${id}')
+                            .on( 'init.dt', function () {
+                                $js
+                            });
+
+}
+JS
+            );
+
+
+        }
+
+
+    }
+
+    protected function getParams()
+    {
+        return $this->_options;
+    }
+
+    protected function initColumns()
+    {
+        if (isset($this->_options['columns'])) {
+            foreach ($this->_options['columns'] as $key => $value) {
+                if (!is_array($value)) {
+                    $value = [
+                            'class' => DataTableColumn::class,
+                            'attribute' => $value,
+                            'label' => Inflector::camel2words($value)
+                    ];
+                }
+                if (isset($value['type'])) {
+                    if ($value['type'] == 'link') {
+                        $value['class'] = LinkColumn::className();
+                        unset($value['type']);
+                    }
+                }
+                if (!isset($value['class'])) {
+                    $value['class'] = DataTableColumn::className();
+                }
+                if (isset($value['class'])) {
+                    $column = \Yii::createObject($value);
+                    $this->_options['columns'][$key] = $column;
+                }
+                if (isSet($value['htmlOptions'])) {
+                    $this->_options['htmlOptions'][$key] = $value['htmlOptions'];
+                }
+            }
+        }
+
+    }
+
+    private function initData() {
+    	if (array_key_exists('data', $this->_options)) {
+    		$data = [];
+
+    		foreach ($this->_options['data'] as $obj) {
+			    $row = [];
+			    foreach ($this->_options['columns'] as $column) {
+				    if ($column instanceof DataTableColumn) {
+					    $value = ArrayHelper::getValue($obj, $column->data);
+					    if (($pos = strrpos($column->data, '.')) !== false) {
+						    $keys = explode('.', $column->data);
+						    $a = $value;
+						    foreach (array_reverse($keys) as $key) {
+							    $a = [$key => $a];
+						    }
+						    $row[$keys[0]] = $a[$keys[0]];
+					    } else {
+						    $row[$column->data] = $value;
+					    }
+				    }
+			    }
+			    if ($row)
+				    $data[] = $row;
+		    }
+		    $this->_options['data'] = $data;
+	    }
+    }
+
+    public function __set($name, $value)
+    {
+        return $this->_options[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return isset($this->_options[$name]) ? $this->_options[$name] : null;
+    }
+
+}
---
+++ DataTableColumn.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018 Herve Guenot
+ * @license MIT
+ * @author Herve Guenot <hguenot@gmail.com>
+ */
+namespace nullref\datatable;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\helpers\Inflector;
+use yii\web\JsExpression;
+
+class DataTableColumn extends Widget
+{
+    /**
+     * @var string the attribute name associated with this column.
+     */
+    public $data;
+
+    /**
+     * @var string label to be displayed in the header.
+     * If it is not set [[\yii\helpers\Inflector::camel2words()]] will be used to get a label.
+     */
+    public $title;
+
+    /**
+     * @var array|null|false Indicating if a filter will be displayed or not.
+     *
+     * - If this property is not set, a text field will be generated as the filter input with attributes defined
+     *   with [[filterInputOptions]].
+     * - If this property is an array, a dropdown list will be generated that uses this property value as
+     *   the list options.
+     * - If you don't want a filter for this data column, set this value to be false.
+     */
+    private $filter;
+
+    /**
+     * @var array the HTML attributes for the filter input fields. This property is used in combination with
+     * the [[filter]] property. When [[filter]] is not set or is an array, this property will be used to
+     * render the HTML attributes for the generated filter input fields.
+     *
+     * Empty `id` in the default value ensures that id would not be obtained from the model attribute thus
+     * providing better performance.
+     *
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $filterInputOptions = ['class' => 'form-control', 'id' => null];
+
+    /** @var JsExpression Javascript (function or expression) used to display the filter */
+    public $renderFilter;
+
+	/** @var JsExpression Javascript (function) used to display the value. */
+    public $render;
+
+    /**
+     * @var array the htmlOptions to apply in each column cells
+     */
+    public $htmlOptions;
+
+    public function init() {
+        parent::init();
+
+        if ($this->data === null) {
+            throw new InvalidConfigException(get_class($this) . '::$data must be set.');
+        }
+
+        if ($this->title === null) {
+            $this->title = Inflector::camel2words($this->attribute);
+        }
+
+        if ($this->render === null) {
+            $this->render= $this->getJsRender();
+        }
+
+        if ($this->renderFilter === null) {
+            $this->renderFilter =  $this->getJsFilter();
+        }
+    }
+
+    public function setAttribute($attribute)
+    {
+        $this->data = $attribute;
+    }
+
+    public function getAttribute()
+    {
+        return $this->data;
+    }
+
+    public function setLabel($label)
+    {
+        $this->title = $label;
+    }
+
+    public function getLabel()
+    {
+        return $this->title;
+    }
+
+	/**
+	 * @return array|false|null
+	 */
+	public function getFilter() {
+		return $this->filter;
+	}
+
+	/**
+	 * @param array|false|null $filter
+	 */
+	public function setFilter($filter): void {
+		$this->filter = $filter;
+	}
+
+    public function getJsFilter()
+    {
+        $jsTitle = Html::encode($this->label);
+        $jsClass = Html::encode($this->filterInputOptions['class']);
+        $jsId = $this->filterInputOptions['id'] ? Html::encode($this->filterInputOptions['id']) : $this->getId();
+        if (is_array($this->filter)) {
+            $select = "jQuery('<select type=\"text\" placeholder=\"Search {$jsTitle}\"></select>')\n" .
+                    "\t.addClass('{$jsClass}')\n" .
+                    "\t.width('100%')\n" .
+                    "\t.attr('id', '{$jsId}')\n" .
+                    "\t.append(jQuery('<option value=\"\"></option>'))";
+
+            foreach ($this->filter as $key => $value) {
+                $key = Html::encode($key);
+                $value = Html::encode($value);
+                $select .= "\n\t.append(jQuery('<option></option>', {\n\t\t"
+	                . "'value': serverSide ? '{$key}' : '{$value}',\n\t\t"
+	                . "'text': '{$value}'\n\t"
+	                . "}))";
+            }
+
+            return new JsExpression("function(table) { var serverSide = table.page.info().serverSide; return {$select}; }");
+        } else if ($this->filter !== false) {
+            return new JsExpression(
+                    "function() {" .
+                    "return jQuery('<input type=\"text\" placeholder=\"Search {$jsTitle}\" />')\n" .
+                    "\t.addClass('{$jsClass}')\n" .
+                    "\t.width('100%')\n" .
+                    "\t.attr('id', '{$jsId}');\n" .
+                    "}"
+            );
+        } else {
+	        return new JsExpression('jQuery()');
+        }
+    }
+
+    public function getJsRender()
+    {
+        $jsTitle = Html::encode($this->label);
+        $jsClass = Html::encode($this->filterInputOptions['class']);
+        $jsId = $this->filterInputOptions['id'] ? Html::encode($this->filterInputOptions['id']) : $this->getId();
+        if (is_array($this->filter)) {
+            $select = "switch (data) {";
+
+            foreach ($this->filter as $key => $value) {
+                $key = Html::encode($key);
+                $value = Html::encode($value);
+                $select .= "\n\tcase '{$key}': return '{$value}';";
+            }
+            $select .= "\n\tdefault: return data;";
+            $select .= "\n}";
+
+            return new JsExpression("function render(data, type, row, meta) { {$select} }");
+        } else {
+            return new JsExpression("function render(data, type, row, meta){ return data; }");
+        }
+    }
+
+}
