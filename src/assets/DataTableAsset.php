3fe9c0e14063b4763d9f0ce67a3ca636402a682e
Chau Yun Pang <pang@palmary.com.hk>
2025-09-30 14:23:49 +0800

    Update Bootstrap Dependencies

--- src/assets/DataTableAsset.php
+++ src/assets/DataTableAsset.php
@@ -14,8 +14,6 @@ class DataTableAsset extends AssetBundle
 {
     const STYLING_DEFAULT = 'default';
     const STYLING_BOOTSTRAP = 'bootstrap';
-    // const STYLING_BOOTSTRAP4 = 'bootstrap4';
-    // const STYLING_BOOTSTRAP5 = 'bootstrap5';
     const STYLING_JUI = 'jqueryui';
 
     public $styling = self::STYLING_DEFAULT;
@@ -36,12 +34,6 @@ class DataTableAsset extends AssetBundle
             case self::STYLING_BOOTSTRAP:
                 $this->depends[] = DataTableBootstrapAsset::class;
                 break;
-            // case self::STYLING_BOOTSTRAP4:
-            //     $this->depends[] = DataTableBootstrap4Asset::class;
-            //     break;
-            // case self::STYLING_BOOTSTRAP5:
-            //     $this->depends[] = DataTableBootstrap5Asset::class;
-            //     break;
             case self::STYLING_DEFAULT:
                 $this->depends[] = DataTableBaseAsset::class;
                 break;
